938038f157fe8318126f2eaa4843b9e5ef4c2bea
Aloys <aloys.oberthuer@gmail.com>
2018-01-17 18:47:24 +0100

    Lessons 148 and 149: removing the ThirdPersonCharacter script

--- btRpg/Assets/aaaRPG/Scripts/Characters/CharacterMovement.cs
+++ btRpg/Assets/aaaRPG/Scripts/Characters/CharacterMovement.cs
@@ -6,13 +6,24 @@ using RPG.CameraUI;
 namespace RPG.Characters
 {
     [RequireComponent(typeof(NavMeshAgent))]
-    [RequireComponent(typeof(ThirdPersonCharacter))]
     public class CharacterMovement : MonoBehaviour
     {
 
-        [SerializeField] float stoppingDistance = 1f;
+        private const float MOVE_NORMALIZE_THRESHOLD = 1f;
 
-        private ThirdPersonCharacter character;   // A reference to the ThirdPersonCharacter on the object
+        [SerializeField] float stoppingDistance = 1f;
+        [SerializeField] float moveSpeedMultiplier = 0.7f;
+        [SerializeField] float animationSpeedMultiplier = 1.2f;
+
+        [SerializeField] float movingTurnSpeed = 360;
+        [SerializeField] float stationaryTurnSpeed = 180;
+
+        float turnAmount;
+        float forwardAmount;
+
+        private Animator animator;
+        private Rigidbody ridigBody;
+
         private NavMeshAgent agent;
 
         private Vector3 clickPoint;
@@ -27,7 +38,10 @@ namespace RPG.Characters
             cameraRaycaster.onMouseOverEnemy += OnMouseOverEnemy;
             cameraRaycaster.onMouseOverWalkable += OnMouseOverWalkable;
 
-            character = GetComponent<ThirdPersonCharacter>();
+            animator = GetComponent<Animator>();
+
+            ridigBody = GetComponent<Rigidbody>();
+            ridigBody.constraints = RigidbodyConstraints.FreezeRotation; // use this instead of bitwise OR for the three axis
 
             agent = GetComponent<NavMeshAgent>();
             agent.updateRotation = false;
@@ -39,11 +53,11 @@ namespace RPG.Characters
         {
             if (agent.remainingDistance > agent.stoppingDistance)
             {
-                character.Move(agent.desiredVelocity);
+                Move(agent.desiredVelocity);
             }
             else
             {
-                character.Move(Vector3.zero);
+                Move(Vector3.zero);
             }
         }
 
@@ -63,6 +77,20 @@ namespace RPG.Characters
             }
         }
 
+        private void OnAnimatorMove()
+        {
+            // we implement this function to override the default root motion.
+            // this allows us to modify the positional speed before it's applied.
+            if (Time.deltaTime > 0)
+            {
+                Vector3 velocity = (animator.deltaPosition * moveSpeedMultiplier) / Time.deltaTime;
+
+                // we preserve the existing y part of the current velocity.
+                velocity.y = ridigBody.velocity.y;
+                ridigBody.velocity = velocity;
+            }
+        }
+
         private void ProcessDirectMovement()
         {
             // read inputs
@@ -72,8 +100,44 @@ namespace RPG.Characters
             Vector3 cameraForward = Vector3.Scale(Camera.main.transform.forward, new Vector3(1, 0, 1)).normalized;
             Vector3 movement = v * cameraForward + h * Camera.main.transform.right;
 
-            character.Move(movement);
+            Move(movement);
+        }
+
+        public void Move(Vector3 move)
+        {
+            SetForwardAndTurn(move);
+            ApplyExtraTurnRotation();
+            UpdateAnimator();
+        }
+
+        private void SetForwardAndTurn(Vector3 move)
+        {
+            // convert the world relative moveInput vector into a local-relative
+            // turn amount and forward amount required to head in the desired direction.
+            if (move.magnitude > MOVE_NORMALIZE_THRESHOLD)
+            {
+                move.Normalize();
+            }
+
+            Vector3 localMove = transform.InverseTransformDirection(move);
+            localMove = Vector3.ProjectOnPlane(localMove, Vector3.up);
+
+            turnAmount = Mathf.Atan2(localMove.x, localMove.z);
+            forwardAmount = localMove.z;
+        }
+
+        void ApplyExtraTurnRotation()
+        {
+            // help the character turn faster (this is in addition to root rotation in the animation)
+            float turnSpeed = Mathf.Lerp(stationaryTurnSpeed, movingTurnSpeed, forwardAmount);
+            transform.Rotate(0, turnAmount * turnSpeed * Time.deltaTime, 0);
+        }
+
+        void UpdateAnimator()
+        {
+            animator.SetFloat("Forward", forwardAmount, 0.1f, Time.deltaTime);
+            animator.SetFloat("Turn", turnAmount, 0.1f, Time.deltaTime);
+            animator.speed = animationSpeedMultiplier;
         }
     }
 }
-
