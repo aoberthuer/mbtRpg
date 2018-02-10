cebb2eb08beb6229b50079a70c9abd8678553d4d
Aloys <aloys.oberthuer@gmail.com>
2018-02-10 15:20:47 +0100

    Lesson 162: AI Behaviour Tree in Unity
    Added State enum for patrolling, chasing, attacking and idle. Enemies chase Player again.

--- btRpg/Assets/aaaRPG/Scripts/Characters/Enemies/EnemyAI.cs
+++ btRpg/Assets/aaaRPG/Scripts/Characters/Enemies/EnemyAI.cs
@@ -1,32 +1,66 @@
 ﻿using UnityEngine;
 
 using RPG.Weapons;
+using System.Collections;
 
 namespace RPG.Characters
 {
+    [RequireComponent(typeof(Character))]
     [RequireComponent(typeof (WeaponSystem))]
     public class EnemyAI : MonoBehaviour
     {
         
         [SerializeField] float chaseRadius = 4f;
 
-        PlayerMovement playerMovement = null;
-        float currentWeaponRange = 2f;
-        bool isAttacking = false;
+        private Character character;
+        private PlayerMovement player;
+
+        private float currentWeaponRange = 2f;
+        private float distanceToPlayer;
+
+        enum State { idle, patrolling, attacking, chasing } // TODO consider extracting State enum
+        State state = State.idle;
 
         private void Start()
         {
-            playerMovement = FindObjectOfType<PlayerMovement>();
+            character = GetComponent<Character>();
+            player = FindObjectOfType<PlayerMovement>();
         }
 
         private void Update()
         {
-            if (playerMovement != null)
+            if (player != null)
             {
-                float distanceToPlayer = Vector3.Distance(playerMovement.transform.position, transform.position);
+                distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
 
                 WeaponSystem weaponSystem = GetComponent<WeaponSystem>();
                 currentWeaponRange = weaponSystem.GetCurrentWeapon().GetMaxAttackRange();
+
+                if (distanceToPlayer > chaseRadius && state != State.patrolling)
+                {
+                    StopAllCoroutines();
+                    state = State.patrolling;
+                }
+                if (distanceToPlayer <= chaseRadius && state != State.chasing)
+                {
+                    StopAllCoroutines();
+                    StartCoroutine(ChasePlayer());
+                }
+                if (distanceToPlayer <= currentWeaponRange && state != State.attacking)
+                {
+                    StopAllCoroutines();
+                    state = State.attacking;
+                }
+            }
+        }
+
+        private IEnumerator ChasePlayer()
+        {
+            state = State.chasing;
+            while (distanceToPlayer >= currentWeaponRange)
+            {
+                character.SetDestination(player.transform.position);
+                yield return new WaitForEndOfFrame();
             }
         }
 
